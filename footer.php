f1d03db4c317ea7939df4dbefcb6bd2e82557023
ankush90412 <ankush90412@gmail.com>
2024-11-10 22:43:34 +0530

    update some features

--- footer.php
+++ footer.php
@@ -1,3 +1,3 @@
 <footer>
-    <p>&copy; 2024 My PHP Website. All rights reserved.</p>
+    <p>&copy; 2024 Akki's PHP Website. All rights reserved.</p>
 </footer>
--- pages/home.php
+++ pages/home.php
@@ -1,5 +1,5 @@
 <img src="images/test.jpg">
 <section>
-    <h2>Welcome to My Website</h2>
+    <h2>Welcome to My php Website</h2>
     <p>This is the home page content. Enjoy browsing our site!</p>
 </section>
